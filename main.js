0c657e53ed76da0ee7b6f7000ea10efa8f8b5c95
Student <student@RU-019.local>
2015-09-14 17:13:49 -0600

    woot

--- main.js
+++ main.js
@@ -1,19 +1,45 @@
-var getName = function(arg1) {
-	var person = {
-		name : 'Luisa',
-		age  : 25,
-	}
-	return person.name
+// var getName = function(arg1) {
+// 	var person = {
+// 		name : 'Luisa',
+// 		age  : 25,
+// 	}
+// 	return person.name
 
+// }
+// // console.log(getName())
+
+// var totalLetters = function(arr) {
+	
+// 	return arr.join("").length;
+
+// }
+// // console.log(totalLetters(['javascript', 'is', 'awesome']))
+
+// var array = ['javascript', 'is', 'awesome']
+// array
+
+// var string = "I like string cheese"
+
+// var keyValue = function(key, value) {
+// 	return {[key]: value};
+// }
+
+// console.log(keyValue('city', 'Denver'))
+var negativeIndex = function(arr, index) {
+	return arr[arr.length + index]
 }
-// console.log(getName())
 
-var totalLetters = function() {
-	console.log(arguments)
-	for ( i =0; i < arguments.length; i++){
-		console.log(arguments.length)
+// console.log(negativeIndex(['a', 'b', 'c', 'd', 'e'], -2))
+
+
+var removeM = function(string) {
+	var array = string.split("")
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] === 'm') {
+			array[i] = ""
+		}
 	}
-
+	return array.join("")
 }
-console.log(totalLetters(['javascript', 'is', 'awesome']))
-
+console.log(removeM('mommy'))
+console.log(removeM('money'))
